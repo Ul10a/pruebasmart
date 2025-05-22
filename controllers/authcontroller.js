652cad5fcda4c9922d20ee1edce66e10ad14e014
Albert Ulloa Mina <142063470+Ul10a@users.noreply.github.com>
2025-05-22 18:37:53 -0500

    Update authcontroller.js

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -124,10 +124,10 @@ exports.postForgotPassword = async (req, res) => {
 
     const { email } = req.body;
 
-    if (!email) {
+    if (!email || typeof email !== 'string') {
       return res.status(400).json({
         success: false,
-        message: 'El correo electrónico es requerido'
+        message: 'El correo electrónico es requerido y debe ser texto'
       });
     }
 
